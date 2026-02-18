0d51ef5dc53d2c7c2c647658a6e7119a7d84c8e6
vnkngr1 <vanyaprekol@gmail.com>
2026-02-18 20:07:09 +0300

    Update Laba1Task1.cpp

---
+++ Laba1Task1.cpp
@@ -0,0 +1,111 @@
+//============================================================================
+// Name        : Laba1Task1.cpp
+// Author      : Garshin Ivan
+// Description : Параллельный поиск простых чисел с Boost.Thread
+//- Реализовать многопоточный алгоритм поиска простых чисел в диапазоне [1, N], используя K потоков.
+//- Каждому потоку передаётся свой поддиапазон, который он проверяет на простые числа.
+//- Главный поток собирает результаты и выводит общее количество найденных простых чисел.
+//- Замерить и сравнить время выполнения программы в однопоточном и многопоточном режимах.
+//============================================================================
+
+#include <boost/thread.hpp>
+#include <iostream>
+
+// Функция-проверка простое ли число
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+// Функция для еденичного потока по поиску простых чисел в заданном диапазоне
+void findPrimesInRange(int start, int end, std::vector<int>& primes, boost::mutex& mutex) {
+    std::vector<int> localPrimes;
+
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            localPrimes.push_back(i);
+        }
+    }
+
+    // Блокируем мьютекс для добавления результатов в общий вектор
+    boost::lock_guard<boost::mutex> lock(mutex);
+    primes.insert(primes.end(), localPrimes.begin(), localPrimes.end());
+}
+
+// Версия верхней функции для одного потока
+void singleThreaded(int N, std::vector<int>& primes) {
+    for (int i = 1; i <= N; ++i) {
+        if (isPrime(i)) {
+            primes.push_back(i);
+        }
+    }
+}
+
+int main() {
+
+	int K = 10; // Количество потоков
+	int N = 1000000; // Верхний порог диапозона
+
+	// Многопоточный режим
+	{
+		std::vector<int> primes;
+		boost::thread_group threads;
+		boost::mutex mutex;
+
+		int rangeSize = N / K;
+		int remainder = N % K;
+
+		auto startTime = std::chrono::high_resolution_clock::now();
+
+		int currentStart = 1;
+		for (int i = 0; i < K; ++i) {
+			int currentEnd = currentStart + rangeSize - 1;
+
+			// Опустошаем остаток по потокам
+			if (remainder > 0) {
+				currentEnd++;
+				remainder--;
+			}
+
+			threads.create_thread(boost::bind(findPrimesInRange,
+											 currentStart,
+											 currentEnd,
+											 boost::ref(primes),
+											 boost::ref(mutex)));
+
+			currentStart = currentEnd + 1;
+		}
+
+		threads.join_all();
+
+		auto endTime = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+		std::cout << "Многопоточный режим" << std::endl;
+		std::cout << "Найдено простых чисел: " << primes.size() << std::endl;
+		std::cout << "Время выполнения: " << duration.count() << " мс" << std::endl;
+	}
+
+	// Однопоточный режим
+	{
+		std::vector<int> primes;
+
+		auto startTime = std::chrono::high_resolution_clock::now();
+		singleThreaded(N, primes);
+		auto endTime = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+		std::cout << "\nОднопоточный режим:\n";
+		std::cout << "Найдено простых чисел: " << primes.size() << "\n";
+		std::cout << "Время выполнения: " << duration.count() << " мс\n";
+	}
+
+	return 0;
+}
